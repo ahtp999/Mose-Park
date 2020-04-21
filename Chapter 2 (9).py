23c4dd1e00d23247f5650b6097dfe71b75552df5
ahtp999 <63177218+ahtp999@users.noreply.github.com>
2020-04-21 01:39:37 -0400

    Create Chapter 2 (9).py

---
+++ Chapter 2 (9).py
@@ -0,0 +1,14 @@
+Python 3.8.1 (v3.8.1:1b293b6006, Dec 18 2019, 14:08:53) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> spam = 5
+>>> if spam == 1:
+    print('Hello')
+elif spam == 2:
+    print('Howdy')
+else:
+    print('Greetings!')
+
+    
+Greetings!
+>>> 
