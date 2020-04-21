0c1f281aac8ad7eb88876aa4acd04bad0140a73f
ahtp999 <63177218+ahtp999@users.noreply.github.com>
2020-04-21 01:54:31 -0400

    Update Assignment 3.py

--- Assignment 3.py
+++ Assignment 3.py
@@ -1,7 +1,7 @@
 Python 3.8.1 (v3.8.1:1b293b6006, Dec 18 2019, 14:08:53) 
 [Clang 6.0 (clang-600.0.57)] on darwin
 Type "help", "copyright", "credits" or "license()" for more information.
->>> Family = ['Annie', 'JS', 'Henry', 'June', 'Ken']
+>>> Family = ['Mose', 'Sion', 'Cai', 'JH', 'JS']
 >>> Hero = ['A', 'B', 'C', 'D']
 >>> import random
 >>> def hello():
@@ -9,5 +9,5 @@ Type "help", "copyright", "credits" or "license()" for more information.
 
 	
 >>> hello()
-JS
+JH
 >>> 
