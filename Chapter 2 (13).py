644155d23dab5b93a019e988003fd0285d2f3bdc
ahtp999 <63177218+ahtp999@users.noreply.github.com>
2020-04-21 01:40:28 -0400

    Create Chapter 2 (13).py

---
+++ Chapter 2 (13).py
@@ -0,0 +1,34 @@
+Python 3.8.1 (v3.8.1:1b293b6006, Dec 18 2019, 14:08:53) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> for i in range(1,11):
+	print(i)
+
+	
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+>>> i = 1
+>>> while i <= 10:
+	print(i)
+	i = i + 1
+
+	
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+>>> 
